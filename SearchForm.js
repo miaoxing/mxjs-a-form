9740ddd4b653952b433e02d922c18fe8b58c5a71
twinh <twinhuang@qq.com>
2020-09-23 14:41:43 +0800

    feat(SearchForm): 增加 filterValues 回调，用于在搜索前更改表单的值

--- SearchForm.js
+++ SearchForm.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {withTable} from '@mxjs/a-table';
 import {Form, Row} from 'antd';
 
-const SearchForm = withTable(({children, table, ...rest}) => {
+const SearchForm = withTable(({children, table, filterValues, ...rest}) => {
   return <Form
     labelCol={{span: 8}} wrapperCol={{span: 16}}
     onValuesChange={(changedValues, allValues) => {
-      table.handleSearch(allValues);
+      const values = filterValues ? filterValues(allValues, changedValues) : allValues;
+      table.handleSearch(values);
     }}
     {...rest}
   >

08e75106b379970e789dd2ee54c94a1cdf3dbd66
twinh <twinhuang@qq.com>
2021-07-01 22:14:03 +0800

    test(a-page, PageAction): 允许自定义页面操作区样式

---
+++ __tests__/FormAction.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FormAction from '../FormAction';
+import {MemoryRouter} from 'react-router';
+import {render} from '@testing-library/react';
+
+describe('FormAction', () => {
+  test('render', async () => {
+    const {container} = render(<MemoryRouter><FormAction/></MemoryRouter>);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('custom offset', async () => {
+    const {container} = render(<MemoryRouter><FormAction wrapperCol={{offset: 8}}/></MemoryRouter>);
+
+    expect(container).toMatchSnapshot();
+  });
+});

b37ec43ddd7cfb75912dcddf8424bb44ee0974f7
twinh <twinhuang@qq.com>
2023-05-22 23:45:31 +0800

    test(a-form, antd5): 更新 snapshot

--- __tests__/FormAction.js
+++ __tests__/FormAction.js
@@ -1,16 +1,29 @@
 import FormAction from '../FormAction';
 import {MemoryRouter} from 'react-router';
 import {render} from '@testing-library/react';
+import {ConfigProvider} from 'antd';
 
 describe('FormAction', () => {
   test('render', async () => {
-    const {container} = render(<MemoryRouter><FormAction/></MemoryRouter>);
+    const {container} = render(
+      <ConfigProvider theme={{hashed: false}}>
+        <MemoryRouter>
+          <FormAction/>
+        </MemoryRouter>
+      </ConfigProvider>,
+    );
 
     expect(container).toMatchSnapshot();
   });
 
   test('custom offset', async () => {
-    const {container} = render(<MemoryRouter><FormAction wrapperCol={{offset: 8}}/></MemoryRouter>);
+    const {container} = render(
+      <ConfigProvider theme={{hashed: false}}>
+        <MemoryRouter>
+          <FormAction wrapperCol={{offset: 8}}/>
+        </MemoryRouter>
+      </ConfigProvider>,
+    );
 
     expect(container).toMatchSnapshot();
   });

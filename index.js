8d0cc5b5690934c196d2fa821b9208af36321bba
twinh <twinhuang@qq.com>
2024-07-02 20:28:33 +0800

    fix: `export default from` 改为标准语法

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-import {Form as AntdForm} from 'antd';
+import { Form as AntdForm } from 'antd';
 
-export Form from './Form';
-export FormItem from './FormItem';
-export FormAction from './FormAction';
-export FormContext from './FormContext';
-export SearchForm from './SearchForm';
-export SearchItem from './SearchItem';
-export Select from './Select';
-export useForm from './useForm';
-export ModalForm from './ModalForm';
-export const FormList = AntdForm.List;
-export const FormErrorList = AntdForm.ErrorList;
+import Form from './Form';
+import FormItem from './FormItem';
+import FormAction from './FormAction';
+import FormContext from './FormContext';
+import SearchForm from './SearchForm';
+import SearchItem from './SearchItem';
+import Select from './Select';
+import useForm from './useForm';
+import ModalForm from './ModalForm';
+
+const FormList = AntdForm.List;
+const FormErrorList = AntdForm.ErrorList;
+
+export {
+  Form,
+  FormItem,
+  FormAction,
+  FormContext,
+  SearchForm,
+  SearchItem,
+  Select,
+  useForm,
+  ModalForm,
+  FormList,
+  FormErrorList,
+};

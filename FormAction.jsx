4ba9b7b34b42735c5bb436ba9ee8f80a05c24e55
twinh <twinhuang@qq.com>
2024-07-15 21:20:18 +0800

    feat(a-form): `FormAction` 默认显示为设卡片视图
    BREAKING CHANGE: `FormAction` 默认显示为设卡片视图

--- FormAction.jsx
+++ FormAction.jsx
@@ -2,28 +2,34 @@ import { CListBtn } from '@mxjs/a-clink';
 import { Button, Form, Space } from 'antd';
 import PropType from 'prop-types';
 import useForm from './useForm';
+import { Fragment } from 'react';
+import { Section } from '@mxjs/a-section';
 
-const FormAction = ({url, list = true, ...props}) => {
+const FormAction = ({url, list = true, variant = 'card', ...props}) => {
   const {loading} = useForm();
+  const Container = variant === 'card' ? Section : Fragment;
 
   return (
-    <Form.Item
-      wrapperCol={{offset: 4, span: 8}}
-      {...props}
-    >
-      <Space>
-        <Button htmlType="submit" type="primary" loading={loading}>
-          提交
-        </Button>
-        {list && (url ? <Button href={url}>返回列表</Button> : <CListBtn/>)}
-      </Space>
-    </Form.Item>
+    <Container>
+      <Form.Item
+        wrapperCol={{offset: 4, span: 8}}
+        {...props}
+      >
+        <Space>
+          <Button htmlType="submit" type="primary" loading={loading}>
+            提交
+          </Button>
+          {list && (url ? <Button href={url}>返回列表</Button> : <CListBtn/>)}
+        </Space>
+      </Form.Item>
+    </Container>
   );
 };
 
 FormAction.propTypes = {
   url: PropType.string,
   list: PropType.bool,
+  variant: PropType.oneOf(['card', 'none']),
 };
 
 export default FormAction;
